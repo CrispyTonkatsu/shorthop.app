b35e23d1a9f44f0ae4a6aa1d05624f139e135b96
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2026-07-06 21:21:32 -0700

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "The flake to build my website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+
+    flake-utils.lib.eachDefaultSystem (
+      local-system:
+      let
+        pkgs = nixpkgs.legacyPackages.${local-system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "shorthop.app flake";
+
+          packages = with pkgs; [
+            # Runtime (only for the local development)
+            bun
+
+            # Linter
+            oxlint
+
+            # Bundler
+            vitejs
+          ];
+        };
+      }
+    );
+}
