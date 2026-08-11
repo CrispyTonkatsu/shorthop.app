71c5130d0a22cf8ab78973d426d8722aef95f65a
Edgar Jose Donoso Mansilla <edguitar.dm@gmail.com>
2026-08-11 00:11:30 -0700

    Removed some stuff

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,6 +1,4 @@
 export default function Blog() {
-  // NOTE: Consider adding a filter functionality for different types of projects
-
   return (
     <div className="flex flex-col min-h-1/2 place-content-center">
       <div className="flex flex-row place-self-center">
